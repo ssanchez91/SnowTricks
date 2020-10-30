ad1fe6376a2e3a9f5f9e747173f17a3155645094
s.sanchez <s.sanchez@iinext.fr>
2020-10-30 17:03:09 +0100

    add __constructor with ServicePicture param to reduce params in method #35

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -15,13 +15,26 @@ use Symfony\Component\Serializer\SerializerInterface;
 class PictureController extends AbstractController
 {
     /**
+     * @var PictureService
+     */
+    private $pictureService;
+
+    /**
+     * PictureController constructor.
+     */
+    public function __construct(PictureService $pictureService)
+    {
+        $this->pictureService = $pictureService;
+    }
+
+    /**
      * @Route("/picture/delete/{id}", name="picture_delete", methods={"DELETE"})
      */
-    public function pictureDelete(Request $request, Picture $picture, PictureService $pictureService): JsonResponse
+    public function pictureDelete(Request $request, Picture $picture): JsonResponse
     {
         if($this->isCsrfTokenValid('delete_', $request->request->get('_token'))){
 
-            if($pictureService->deletePicture($picture))
+            if($this->pictureService->deletePicture($picture))
             {
                 return new JsonResponse(['success' => 1, "default_picture" => false ]);
             }
@@ -39,11 +52,11 @@ class PictureController extends AbstractController
     /**
      * @Route("/picture/default/{id}", name="picture_default", methods={"POST"})
      */
-    public function pictureDefault(Request $request, Picture $picture, PictureService $pictureService): JsonResponse
+    public function pictureDefault(Request $request, Picture $picture): JsonResponse
     {
         if($this->isCsrfTokenValid('default_', $request->request->get('_token')))
         {
-            if($pictureService->defaultPicture($picture))
+            if($this->pictureService->defaultPicture($picture))
             {
                 return new JsonResponse(['success' => 1, "default_picture" => "updated" ]);
             }
@@ -62,13 +75,13 @@ class PictureController extends AbstractController
     /**
      * @Route("/picture/add/{id}", name="picture_add", methods={"POST"})
      */
-    public function pictureAdd(Request $request, Figure $figure , PictureService $pictureService, FileUploaderService $fileUploaderService, SerializerInterface $serializerInterface): JsonResponse
+    public function pictureAdd(Request $request, Figure $figure, FileUploaderService $fileUploaderService, SerializerInterface $serializerInterface): JsonResponse
     {
         $new_picture = $request->files->get('file');
 
         if(!empty($new_picture) && ($new_picture->guessExtension() == 'png' || $new_picture->guessExtension() == 'jpeg' || $new_picture->guessExtension() == 'jpg' ))
         {
-            $picture = $pictureService->addPicture($new_picture, $figure);
+            $picture = $this->pictureService->addPicture($new_picture, $figure);
             if($picture)
             {
                 return new JsonResponse(['success' => 1, "add_picture" => $serializerInterface->serialize($picture, 'json', ['groups'=> 'tricks:read']), 'figure' => $figure->getId() ]);
